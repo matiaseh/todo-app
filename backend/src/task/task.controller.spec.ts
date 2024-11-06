bc71a2b0556fe4b34b8b875c955aa30c18473b9f
Matias Hynnä <hynna.matias@gmail.com>
2024-11-06 09:26:59 +0200

    Add basic tests for task routes

--- backend/src/task/task.controller.spec.ts
+++ backend/src/task/task.controller.spec.ts
@@ -1,7 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
-import { Task, TaskStatus } from './task.type';
+import { AddTaskDto, Task, TaskStatus, UpdateTaskDto } from './task.type';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 describe('TaskController', () => {
   let taskController: TaskController;
@@ -15,7 +16,7 @@ describe('TaskController', () => {
     taskController = app.get<TaskController>(TaskController);
   });
 
-  describe('getTasks', () => {
+  describe('GET Tasks', () => {
     it('should return default tasks', () => {
       const defaultTasks: Task[] = [
         {
@@ -41,6 +42,54 @@ describe('TaskController', () => {
         },
       ];
       expect(taskController.getTasks()).toEqual(defaultTasks);
+    });
+
+    it('should have correct length', () => {
+      const tasks = taskController.getTasks();
+      expect(tasks).toHaveLength(3);
+      expect(tasks[0].name).toBe('Vacuum the living room');
+    });
+  });
+
+  describe('POST Task', () => {
+    it('should add a new task', () => {
+      const newTask: AddTaskDto = { name: 'Change winter tyres' };
+      const task = taskController.addTask(newTask);
+
+      expect(task).toBeDefined();
+      expect(task.id).toBe(4);
+      expect(task.name).toBe(newTask.name);
+      expect(task.status).toBe(TaskStatus.Todo);
+    });
+
+    it('should throw error if name is invalid', () => {
+      const newTask: AddTaskDto = { name: '' };
+
+      expect(() => {
+        taskController.addTask(newTask);
+      }).toThrow(BadRequestException);
+      expect(() => {
+        taskController.addTask({ name: null as any });
+      }).toThrow(BadRequestException);
+    });
+  });
+
+  describe('PATCH Task', () => {
+    it('should update the task status', () => {
+      const updateData: UpdateTaskDto = { status: TaskStatus.Done };
+      const taskToUpdate = taskController.getTasks().find((t) => t.id === 1);
+
+      expect(taskToUpdate.status).toBe(TaskStatus.Todo);
+      taskController.updateTask(taskToUpdate.id, updateData);
+      expect(taskToUpdate.status).toBe(TaskStatus.Done);
+    });
+
+    it('should throw error if task not found', () => {
+      const updateData: UpdateTaskDto = { status: TaskStatus.Done };
+
+      expect(() => {
+        taskController.updateTask(999, updateData);
+      }).toThrow(NotFoundException);
     });
   });
 });
